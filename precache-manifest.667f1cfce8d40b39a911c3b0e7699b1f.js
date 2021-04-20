ee3ab794fc24ca31a0a9cdd995afbd11db052ce9
youhaosuda <943514589@qq.com>
2021-04-20 23:42:02 +0800

    deploy

---
+++ precache-manifest.667f1cfce8d40b39a911c3b0e7699b1f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdb615730fd179d8bd2",
+    "url": "/gift-website/css/app.8cc6f1ce.css"
+  },
+  {
+    "revision": "013fe7cd9d6586cc20b86768f23283bd",
+    "url": "/gift-website/img/birthday.013fe7cd.png"
+  },
+  {
+    "revision": "f445c4b7eec721298d98777ad0ba31d6",
+    "url": "/gift-website/img/heart.f445c4b7.png"
+  },
+  {
+    "revision": "2e1777df54397c726e0f2af3f68b5692",
+    "url": "/gift-website/img/tree.2e1777df.png"
+  },
+  {
+    "revision": "eb909e116a86dc426a631324f9959a79",
+    "url": "/gift-website/index.html"
+  },
+  {
+    "revision": "5bdb615730fd179d8bd2",
+    "url": "/gift-website/js/app.f8600a92.js"
+  },
+  {
+    "revision": "76724d8786a0e02f7a8d",
+    "url": "/gift-website/js/chunk-vendors.3880dde6.js"
+  },
+  {
+    "revision": "f4824579ca8e2c25a6d5a74c4a19b4b2",
+    "url": "/gift-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gift-website/robots.txt"
+  }
+]);
